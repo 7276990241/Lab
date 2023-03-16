9e8830c20c185f4343ec2b404b4b52cdeec57df4
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-03-16 13:51:07 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,18 @@
+class Sum
+{
+public static void main(String args[])
+{
+int a=20,b=15,res;
+res=a+b;
+System.out.println("The Addition result is:"+res);
+
+res=a-b;
+System.out.println("The Subtraction result is:"+res);
+
+res=a*b;
+System.out.println("The Multiplication result is:"+res);
+
+res=a/b;
+System.out.println("The Division result is:"+res);
+}
+}
