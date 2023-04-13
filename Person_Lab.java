e9baf7ec8c69fa009a0c3c644d26f5831ae8e83d
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-04-13 13:33:12 +0530

    Add files via upload

---
+++ Person_Lab.java
@@ -0,0 +1,29 @@
+package abstraction;
+
+interface Person {
+	  public void speak();
+	}
+
+	class Student implements Person {
+	  public void speak() {
+	    System.out.println("I am a student.");
+	  }
+	}
+
+	class Teacher implements Person {
+	  public void speak() {
+	    System.out.println("I am a teacher.");
+	  }
+	}
+
+	//public interface LabWord_Person {
+	public class Person_Lab {
+	  public static void main(String[] args) {
+	    Person student = new Student();
+	    student.speak(); // "I am a student."
+	    
+	    Person teacher = new Teacher();
+	    teacher.speak(); // "I am a teacher."
+	  }
+}
+
