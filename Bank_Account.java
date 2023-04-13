bb197c068b517df40759c3bbeb1bbaeced36e326
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-04-13 13:32:31 +0530

    Add files via upload

---
+++ Bank_Account.java
@@ -0,0 +1,56 @@
+package abstraction;
+
+abstract class BankAccount {
+    private int accountNumber;
+    protected double balance;
+
+    public BankAccount(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+}
+
+class CheckingAccount extends BankAccount {
+    public CheckingAccount(int accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        double currentBalance = getBalance();
+        double newBalance = currentBalance + amount;
+        super.balance = newBalance;
+        System.out.println(" Rs:" + amount + " deposited. \n New balance: " + newBalance);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        double currentBalance = getBalance();
+        if (currentBalance < amount) {
+            System.out.println("Insufficient funds. Withdrawal not possible.");
+        } else {
+            double newBalance = currentBalance - amount;
+            super.balance = newBalance;
+            System.out.println("\n Rs:" + amount + " withdrawn.\n New balance: " + newBalance);
+        }
+    }
+}
+
+public class Bank_Account {
+    public static void main(String[] args) {
+        CheckingAccount account = new CheckingAccount(112233, 10000);
+        account.deposit(5500);
+        account.withdraw(2200);
+    }
+}
