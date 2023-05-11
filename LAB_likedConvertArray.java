3fe9b572c7983984e65965071f80b1cfb7276e39
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-05-11 12:40:40 +0530

    Add files via upload

---
+++ LAB_likedConvertArray.java
@@ -0,0 +1,27 @@
+
+//Write a Java program to convert a linked list to an array list.
+
+package collections;
+
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class LAB_likedConvertArray {
+	public static void main(String[] args) {
+		
+		// Create a linked list
+        LinkedList<String> linkedList = new LinkedList<String>();
+        linkedList.add("Ajay");
+        linkedList.add("Aishwarya");
+        linkedList.add("Susmit");
+
+        // Convert linked to array
+        ArrayList<String> arrayList = new ArrayList<String>(linkedList);
+
+        // Print the array list
+        System.out.println("Array List: " + arrayList);
+		
+	}
+
+}
+
