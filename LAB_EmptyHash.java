c3b602fdde4f5ddceee8dd719ae03327f52201f2
Priyanka Kate <127471859+7276990241@users.noreply.github.com>
2023-05-15 12:41:59 +0530

    Add files via upload

---
+++ LAB_EmptyHash.java
@@ -0,0 +1,26 @@
+package collections;
+
+
+//Write a Java program to empty an hash set.
+import java.util.HashSet;
+
+public class LAB_EmptyHash {
+	public static void main(String[] args) {
+		
+	//creating a hash set
+		HashSet<String> hs = new HashSet<String>();
+		
+		//adding the to hash set
+		hs.add("Python");
+		hs.add("Java");
+		hs.add("HTMl");
+		
+		 System.out.println("Hash Set before clearing:"+hs);
+
+	        // Clearing the HashSet
+	        hs.clear();
+
+	        System.out.println("Hash Set after clearing:"+hs);	
+
+	}	
+}
